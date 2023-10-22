431951611de619698c109999020121e4b5385bcc
John Doe <Example@email.com>
2023-10-22 14:20:07 -0500

    Added a bit of documentation and Colleen's node.js app. Not fully functional

--- src/App.js
+++ src/App.js
@@ -9,42 +9,73 @@ import { wordList } from './wordList';
 //NOT FULLY FUNCTIONING YET!
 const createLetterStack = () => {
 
+  //Creates a div to store buttons in question
   const buttonContainer = document.createElement('div');
+  //Makes new elements part of class 'button-container'
   buttonContainer.classList.add('button-container');
 
+  //Max num of rows? 
   const rows = 5;
 
+  //For each row...
   for (let i = 0; i < rows; i++) {
 
+    //Do we need to make new buttonRow const every run? Just move it under the const rows? 
+    
+    //Create a new button row element
     const buttonRow = document.createElement('div');
+
+    //Makes new elemnts part of class 'button-row'
     buttonRow.classList.add('button-row');
+    
+    //Adds a buttonRow to the html document????
     buttonContainer.appendChild(buttonRow);
 
   }
 
+  //Adds the buttonContainer to the body
   document.body.appendChild(buttonContainer);
   
 };
 
+
+//The app in question, the main function???
 const App = () => {
+
+
+  /*First parameter is object we want to watch, second parameter is the 
+  function used to update the state. useState is just the default state? 
+  
+  Parameter passed to the useState method also determines type of the value???
+  useState is a react hook, specificially a state hook, updates and reacts when data or properties change
+  */
+ 
+  //Since function is the same, can't we just have them call the same function?
   const [randomWord1, setRandomWord1] = useState('');
   const [randomWord2, setRandomWord2] = useState('');
+
   const [pressedLetters, setPressedLetters] = useState('');
   const [submitList, setSubmitList] = useState([]);
 
+
   //RANDOMIZE MORE!
+  //Shuffles a string
   const shuffleWord = (word) => {
     const shuffledWord = word.split('').sort(() => Math.random() - .5).join('');
     return shuffledWord;
   };
 
+
   useEffect(() => {
 
+    //Creates rows of letters in html
     createLetterStack();
 
+    //Defines setRandomWord() : Gets random word from wordList.js, shuffles it around, and updates the state. 
     setRandomWord1(shuffleWord(wordList[Math.floor(Math.random() * wordList.length)]));
     setRandomWord2(shuffleWord(wordList[Math.floor(Math.random() * wordList.length)]));
 
+    //No idea, Joseph help me! 
     console.log(randomWord1);
     console.log(randomWord2);
 
@@ -52,6 +83,8 @@ const App = () => {
 
   }, []);
 
+
+  //Everything after this is gibberish to me. I'd appreciate some help. 
   const handleLetterClick = (letter) => {
     setPressedLetters((prevLetters) => prevLetters + letter);
   };
@@ -59,6 +92,7 @@ const App = () => {
   //searches for word played from index
   const wordSearch = () => {
 
+    
     if (wordList.includes(pressedLetters.toUpperCase())) {
 
       alert('Match found!');
@@ -74,10 +108,11 @@ const App = () => {
       alert('No match found.');
 
     }
-
+    
     setPressedLetters('');
 
   };
+
 
   const generateLetterTiles = (word, onClick) => {
 
